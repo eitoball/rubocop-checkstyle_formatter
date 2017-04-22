73114470b6fb5db2183d3ebcc384dfadf22929eb
Eito Katagiri <eitoball@gmail.com>
2017-04-22 23:48:23 +0900

    fix Lint/UnneededSplatExpansion

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), *%w{ .. lib }))
+$LOAD_PATH.unshift(File.absolute_path('../../lib', __FILE__))
 require 'rspec'
 require 'rubocop'
 require 'rubocop/formatter/checkstyle_formatter'
